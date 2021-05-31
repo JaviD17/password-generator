780721633f5a1f70eb2168cd0e89131ab7f0c68a
Javier Dominguez II <Javi7057@gmail.com>
2021-05-30 23:58:55 -0500

    finished project

--- assets/script.js
+++ assets/script.js
@@ -50,7 +50,26 @@ var charTypeUpper = function () {
     }
 };
 
-var numChoice = function() {
+var symFun = function () {
+    var choiceType = prompt("Do you want to include symbols? Types yes or no");
+
+    choiceType = choiceType.toLowerCase();
+    console.log(choiceType);
+
+    if (choiceType === "" || choiceType === null) {
+        var choiceType = alert("Incorrect answer. Try again!");
+        return charTypeUpper();
+    }
+
+    if (choiceType === "yes") {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
+var numChoice = function () {
     var choiceType = prompt("Do you want to include numeric values? Types yes or no");
 
     choiceType = choiceType.toLowerCase();
@@ -73,12 +92,113 @@ var generatePassword = function () {
     var passwordLength = passLength();
     var charChoiceLower = charTypeLower();
     var charChoiceUpper = charTypeUpper();
-    var numVal = numChoice ();
+    var symChoice = symFun();
+    var numVal = numChoice();
 
     // PasswordLength has a value 8 - 128
     // Other functions return true for yes and false for no
+    // character sets
+    var lowerSet = "abcdefghijklmnopqrstuvwxyz";
+    var upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var symbolSet = " !&quot;#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    var numSet = "0123456789";
+    var retVal = "";
 
-    return 0;
+    // All four questions are yes
+    if (charChoiceLower === true && charChoiceUpper === true && symChoice === true && numVal === true) {
+        for (var i = 0; i < (passwordLength + 1); i++) {
+            var charSet = lowerSet.concat(upperSet, symbolSet, numSet);
+            retVal += (charSet.charAt(Math.floor(Math.random() * 96) + 1));
+        }
+    }
+
+
+    // no numbers
+    if (charChoiceLower === true && charChoiceUpper === true && symChoice === true && numVal === false) {
+        for (var i = 0; i < (passwordLength + 1); i++) {
+            var charSet = lowerSet.concat(upperSet, symbolSet);
+            retVal += (charSet.charAt(Math.floor(Math.random() * 96) + 1));
+        }
+    }
+
+    // no symbols
+    if (charChoiceLower === true && charChoiceUpper === true && symChoice === false && numVal === true) {
+        for (var i = 0; i < (passwordLength + 1); i++) {
+            var charSet = lowerSet.concat(upperSet, numSet);
+            retVal += (charSet.charAt(Math.floor(Math.random() * 96) + 1));
+        }
+    }
+
+    // only lower case
+    if (charChoiceLower === true && charChoiceUpper === false && symChoice === false && numVal === false) {
+        for (var i = 0; i < (passwordLength + 1); i++) {
+            var charSet = lowerSet;
+            retVal += (charSet.charAt(Math.floor(Math.random() * 96) + 1));
+        }
+    }
+
+    // no upper case
+    if (charChoiceLower === true && charChoiceUpper === false && symChoice === true && numVal === true) {
+        for (var i = 0; i < (passwordLength + 1); i++) {
+            var charSet = lowerSet.concat(symbolSet, numSet);
+            retVal += (charSet.charAt(Math.floor(Math.random() * 96) + 1));
+        }
+    }
+
+    // no upper case or numbers
+    if (charChoiceLower === true && charChoiceUpper === false && symChoice === true && numVal === false) {
+        for (var i = 0; i < (passwordLength + 1); i++) {
+            var charSet = lowerSet.concat(symbolSet);
+            retVal += (charSet.charAt(Math.floor(Math.random() * 96) + 1));
+        }
+    }
+
+    // no lower case or symbols
+    if (charChoiceLower === false && charChoiceUpper === true && symChoice === false && numVal === true) {
+        for (var i = 0; i < (passwordLength + 1); i++) {
+            var charSet = upperSet.concat(numSet);
+            retVal += (charSet.charAt(Math.floor(Math.random() * 96) + 1));
+        }
+    }
+
+    // only upper case
+    if (charChoiceLower === false && charChoiceUpper === true && symChoice === false && numVal === false) {
+        for (var i = 0; i < (passwordLength + 1); i++) {
+            var charSet = upperSet;
+            retVal += (charSet.charAt(Math.floor(Math.random() * 96) + 1));
+        }
+    }
+
+    // no lower case
+    if (charChoiceLower === false && charChoiceUpper === true && symChoice === true && numVal === true) {
+        for (var i = 0; i < (passwordLength + 1); i++) {
+            var charSet = upperSet.concat(symbolSet, numSet);
+            retVal += (charSet.charAt(Math.floor(Math.random() * 96) + 1));
+        }
+    }
+
+    // only symbols
+    if (charChoiceLower === false && charChoiceUpper === false && symChoice === true && numVal === false) {
+        for (var i = 0; i < (passwordLength + 1); i++) {
+            var charSet = symbolSet;
+            retVal += (charSet.charAt(Math.floor(Math.random() * 96) + 1));
+        }
+    }
+
+    // only numbers
+    if (charChoiceLower === false && charChoiceUpper === false && symChoice === false && numVal === true) {
+        for (var i = 0; i < (passwordLength + 1); i++) {
+            var charSet = numSet;
+            retVal += (charSet.charAt(Math.floor(Math.random() * 96) + 1));
+        }
+    }
+
+    // all four questions are no
+    if (charChoiceLower === false && charChoiceUpper === false && symChoice === false && numVal === false) {
+        alert("No characters to choose from. You need at least one yes. Try again!");
+        generatePassword();
+    }
+    return retVal;
 };
 
 // Get references to the #generate element
